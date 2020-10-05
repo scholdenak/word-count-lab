c8b0d8cebfbc5079c641635a64b74e80986a42d4
vagrant <vagrant@vagrant.vm>
2020-10-05 22:58:07 +0000

    Broke down text to words in list

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,14 @@
 # put your code here.
+
+def count_words(filename):
+    
+    text = open(filename)
+    all_words = []
+
+    for line in text:
+        line = line.rstrip()
+        words = line.split(" ")
+        print("words: ", words)
+        
+        
+    print("ALL WORDS: ", all_words)
